97bf2b7fe76e534a659fc4b989a914a7f4125ffa
Roberto <rhobis@gmail.com>
2018-12-27 12:39:14 +0100

    added package description

---
+++ R/package-bootstrapFP.R
@@ -0,0 +1,23 @@
+#' bootstrapFP: Bootstrap Algorithms for Finite Population Inference
+#' 
+#' Perform bootstrap variance estimation of the Horvitz-Thompson total estimator
+#' in finite population sampling with equal or unequal probabilities.
+#' 
+#' 
+#' 
+#' @docType package
+#'
+#' @name bootstrapFP
+#' 
+#'
+#'
+#' @references
+#' Mashreghi Z.; Haziza D.; Léger C., 2016. A survey of bootstrap methods in 
+#' finite population sampling. Statistics Surveys 10 1-52.
+#'
+#'
+#' @importFrom stats lm pnorm setNames coef model.matrix residuals
+#'
+#'
+NULL
+

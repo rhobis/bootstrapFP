61c9ce58e9989580680fd6b33d5ec163c2a3cf32
Roberto S <rhobis@gmail.com>
2023-08-26 11:11:01 +0200

    added the alias {package}-package to pkg Rd via the roxygen2 sentinel

---
+++ R/bootstrapFP-package.R
@@ -0,0 +1,20 @@
+#' bootstrapFP: Bootstrap Algorithms for Finite Population Inference
+#' 
+#' Perform bootstrap variance estimation of the Horvitz-Thompson total estimator
+#' in finite population sampling with equal or unequal probabilities.
+#' 
+#' 
+#' 
+#' 
+#'
+#'
+#' @references
+#' Mashreghi Z.; Haziza D.; Léger C., 2016. A survey of bootstrap methods in 
+#' finite population sampling. Statistics Surveys 10 1-52.
+#'
+#'
+#' @importFrom stats lm pnorm setNames coef model.matrix residuals
+#'
+#'@aliases bootstrapFP-package NULL
+'_PACKAGE'
+
